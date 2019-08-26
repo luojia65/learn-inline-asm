710075bf848b6dd46b425bb8db4d768c5598c7c6
luojia65 <me@luojia.cc>
2019-08-26 22:07:28 +0800

    Check ASCII

--- sse41-check-ascii-one.rs
+++ sse41-check-ascii-one.rs
@@ -1,9 +1,10 @@
 #![feature(asm, global_asm)]
 global_asm! { r#"
+    .section	__DATA,__const
 var_high_bit:
+    .long 0x80808080 
     .long 0x80808080
-    .long 0x80808080
-    .long 0x80808080
+    .long 0x80808080 
     .long 0x80808080
 "# }
 
@@ -13,17 +14,24 @@ fn main() {
     println!("{:?}", ans);
 }
 
-fn process(src: &[u8]) -> u64 {
-    let ans: u64 = 1;
+fn process(src: &[u8]) -> bool {
+    let ans: bool;
     unsafe { asm!("
-        movdqu xmm2, [r9]
+        lea rax, [rip + var_high_bit]
+        movdqu xmm2, [rax]
         movdqu xmm0, [r8]
         movdqu xmm1, [r8+10h]
         por xmm0, xmm1
         ptest xmm0, xmm2
-    ":
+        jz ascii_validate_success
+        mov rax, 0
+        jmp ascii_validate_end
+    ascii_validate_success:
+        mov rax, 1
+    ascii_validate_end:
+    ":"={rax}"(ans)
     :"{r8}"(src.as_ptr())
-    :
+    :"rax","r8","xmm0","xmm1","xmm2"
     :"intel") };
     ans
 }
