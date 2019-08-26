e1259b2944b4f1b8d58094173eeab610bd13a4ab
luojia65 <me@luojia.cc>
2019-08-26 22:37:42 +0800

    Bug fix

--- sse2-validate-same.rs
+++ sse2-validate-same.rs
@@ -18,17 +18,17 @@ fn process(src: &[u8]) -> u64 {
         movdqa  xmm0, [r8+10h]
         pcmpeqb xmm0, xmm1
         pmovmskb    r10, xmm0
-        shl r11, 10h
+        shl r10, 10h
         or  r11, r10
         movdqa  xmm0, [r8+20h]
         pcmpeqb xmm0, xmm1
         pmovmskb    r10, xmm0
-        shl r11, 10h
+        shl r10, 20h
         or  r11, r10
         movdqa  xmm0, [r8+30h]
         pcmpeqb xmm0, xmm1
         pmovmskb    r10, xmm0
-        shl r11, 10h
+        shl r10, 30h
         or  r11, r10
     ":"={r11}"(ans)
     :"{r8}"(src.as_ptr()), "{r9}"(msk.as_ptr())
