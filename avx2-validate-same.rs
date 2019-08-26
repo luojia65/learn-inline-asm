f5f7e5778fba7d528cc73c05b0476687cba6204d
luojia65 <me@luojia.cc>
2019-08-26 20:52:52 +0800

    Validate same byte (avx2)

---
+++ avx2-validate-same.rs
@@ -0,0 +1,27 @@
+#![feature(asm)]
+
+fn main() {
+    let src = b" important stuff important stuff important stuff important stuff";
+    let ans = process(src);  
+    println!("{:064b}", ans);
+}
+
+fn process(src: &[u8]) -> u64 {
+    let msk = b"iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+    let ans: u64;
+    unsafe { asm!("
+        vmovdqu ymm0, [r8]
+        vmovdqu ymm1, [r9]
+        vpcmpeqb ymm0, ymm0, ymm1
+        vpmovmskb r10, ymm0
+        vmovdqu ymm0, [r8]
+        vpcmpeqb ymm0, ymm0, ymm1
+        vpmovmskb r11, ymm0
+        shl r11, 20h
+        or r11, r10
+    ":"={r11}"(ans)
+    :"{r8}"(src.as_ptr()), "{r9}"(msk.as_ptr())
+    :"xmm0", "xmm1", "r8", "r9", "r10", "r11"
+    :"intel") };
+    ans
+}
