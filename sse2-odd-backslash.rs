0021c1c858b9f8c6e453dee5542c723fde94550d
luojia65 <me@luojia.cc>
2019-08-27 10:19:34 +0800

    faster

--- sse2-odd-backslash.rs
+++ sse2-odd-backslash.rs
@@ -3,13 +3,14 @@ global_asm! { r#"
     .section	.data, ""
 sixteen_backslashes:
     .zero 16, 0x5C
-even_bits_64:
-    .zero 16, 0x55
 "# }
 
 fn main() {
     let src = br#" important \\\\\ i\portant \\\\f important \\\ff important \\uff"#;
-    let ans = process(src);  
+    let ans = process(src); 
+    for i in 0..100_000_000 - 1 {
+        process(src);
+    } 
     println!("{:064b}", ans);
 }
 
@@ -40,7 +41,7 @@ fn process(src: &[u8]) -> u64 {
         shl r8, 1
         not r8
         and r8, r10         # r8: start_edges
-        mov r9, [rip + even_bits_64]    # r9: even_start_mask
+        mov r9, 0x5555555555555555    # r9: even_start_mask
         mov rax, r8
         and rax, r9         # rax: even_starts
         not r9
@@ -50,7 +51,7 @@ fn process(src: &[u8]) -> u64 {
         not r10             # r10: !bs_bits
         and rax, r10        # rax: even_carry_ends
         and r9, r10         # r9: odd_carry_ends
-        mov r10, [rip + even_bits_64] # r10: even_bits
+        mov r10, 0x5555555555555555 # r10: even_bits
         and r9, r10         # r9: odd_start_even_end
         not r10
         and rax, r10        # rax: even_start_odd_end
