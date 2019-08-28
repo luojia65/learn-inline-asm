c06c0add444d4b626f39e22e4860120219ae9bb9
luojia65 <me@luojia.cc>
2019-08-28 16:32:41 +0800

    not enough registers

--- sse-utf8-continuations.rs
+++ sse-utf8-continuations.rs
@@ -24,44 +24,49 @@ fn main() {
 fn process(src: &[u8]) -> () {
     let outh: u64; let outl: u64;
     unsafe { asm!("
-        mov rcx, 0
-        pxor xmm0, xmm0 # xmm0: prev_carries
+        pxor xmm0, xmm0 # xmm0: prev_carried_continuations
         pxor xmm1, xmm1 # xmm1: has_error 
-process_loop:
-        movdqu xmm2, [rdi + rcx] # xmm2: current_bytes
-        movdqa xmm3, xmm2
+        pxor xmm2, xmm2 # xmm2: prev_high_nibbles
+        mov rcx, 0
+// process_loop:
+        movdqu xmm3, [rdi + rcx] # xmm3: current_bytes
+        movdqa xmm4, xmm3
     # check unicode max 0xf4 into has_error
-        psubusb xmm3, [rip + sixteen_unicode_max]
-        por xmm1, xmm3
+        psubusb xmm4, [rip + sixteen_unicode_max]
+        por xmm1, xmm4
     # get continuation lengths
-        movdqa xmm3, xmm2
-        psrlq xmm3, 4
-        pand xmm3, [rip + lo_nibble_filter] # xmm3: hi nibble now 
-        movdqa xmm4, [rip + continuation_length]
-        pshufb xmm4, xmm3 # xmm4: initial lengths
+        movdqa xmm4, xmm3
+        psrlq xmm4, 4
+        pand xmm4, [rip + lo_nibble_filter] # xmm4: hi nibble cur 
+        movdqa xmm5, [rip + continuation_length]
+        pshufb xmm5, xmm4 # xmm5: initial lengths
     # get carried continuations
-        movdqa xmm5, xmm4
-        palignr xmm5, xmm0, 15 
-        psubusb xmm5, [rip + sixteen_u8_ones] # xmm5: right1
-        paddb xmm5, xmm4 # xmm5: sum
-        movdqa xmm6, xmm5 # xmm6: sum
-        palignr xmm6, xmm0, 14 
-        psubusb xmm6, [rip + sixteen_u8_twos] # xmm6: right2
-        paddb xmm5, xmm6 # xmm5: carried_continuations 
+        movdqa xmm6, xmm5
+        palignr xmm6, xmm0, 15 
+        psubusb xmm6, [rip + sixteen_u8_ones] # xmm6: right1
+        paddb xmm6, xmm5 # xmm6: sum
+        movdqa xmm7, xmm6 # xmm7: sum
+        palignr xmm7, xmm0, 14 
+        psubusb xmm7, [rip + sixteen_u8_twos] # xmm7: right2
+        paddb xmm6, xmm7 # xmm6: carried_continuations 
+        movdqa xmm0, xmm6 # xmm0: carried_continuations
     # check continuations
-        pxor xmm6, xmm6 # xmm6: 0
-        pcmpgtb xmm5, xmm4 # xmm5: eq_1
-        pcmpgtb xmm4, xmm6 # xmm4: eq_2
-        pcmpeqb xmm4, xmm5 # xmm4: overunder
-        por xmm1, xmm4 
-    
-
+        pxor xmm7, xmm7 # xmm7: 0
+        pcmpgtb xmm6, xmm5 # xmm6: eq_1
+        pcmpgtb xmm5, xmm7 # xmm5: eq_2
+        pcmpeqb xmm5, xmm6 # xmm5: overunder
+        por xmm1, xmm5 
+    # get offset current bytes
+        palignr xmm6, xmm0, 15 
+        // ???
+    # move current high nibbles to prev high nibbles
+        movdqa xmm2, xmm4
         // add rcx, 64
         // cmp rcx, rsi
         // jb process_loop
 
-        movq rax, xmm4
-        pextrq rdx, xmm4, 1
+        movq rax, xmm1
+        pextrq rdx, xmm1, 1
     ":"={rax}"(outh),"={rdx}"(outl)
     :"{rdi}"(src.as_ptr()), "{rsi}"(src.len())
     :"xmm0","rcx" // todo
