b18addc7ccc94b6951f1810254ee7ee5493e0bc4
luojia65 <me@luojia.cc>
2019-09-01 13:21:37 +0800

    RISCV no_std main

---
+++ riscv-main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[export_name = "main"]
+pub fn main() -> ! {
+    let a: u32 = 0x12345678;
+    let b: u32 = 0x90ABCDEF;
+    if a + b > 0x87654321 {
+        loop {}
+    }
+    loop {}
+}
