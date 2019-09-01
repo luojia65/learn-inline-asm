5229dcb4daab792a8ed4a04fa0fbb541a5975c56
luojia65 <me@luojia.cc>
2019-09-01 16:50:27 +0800

    Update check speed

--- avx2-utf8-check-speed.rs
+++ avx2-utf8-check-speed.rs
@@ -31,12 +31,15 @@ second_min_mask:
     .quad 0x90A07F7F80808080
     .quad 0x8080808080808080
     .quad 0x90A07F7F80808080
+all_ff_bytes:
+    .zero 32, 0xFF
+    .zero 32, 0x00
 "# }
 
 // This program does not check for illegal 0xC0 and 0xC1 chars
 fn main() {
     let mut src: Vec<u8> = Vec::new();
-    for _ in 1..=1024*1024*1024 {
+    for _ in 1..=16*1024*1024 {
         src.push(0x01);
     }
     println!("Size: {} bytes", src.len());
@@ -66,10 +69,12 @@ fn process(src: &[u8]) -> bool {
     # ymm8: tmp
     # ymm9: zero
         vzeroall
-        mov rcx, 0
+        mov rcx, 32
+        cmp rsi, 32
+        jb process_tail_32
 process_loop:
     # load cur_raw 
-        vlddqu ymm4, [rdi + rcx]
+        vlddqu ymm4, [rdi + rcx - 32]
     # check unicode max 0xf4 into has_error
         vpsubusb ymm5, ymm4, [rip + all_f4_bytes]
         vpor ymm0, ymm5, ymm0
@@ -121,6 +126,54 @@ process_loop:
         add rcx, 32
         cmp rcx, rsi
         jb process_loop
+        je process_end
+process_tail_32:
+    # clean last (rsi - rcx) bytes of ymm4 to zero 
+        vlddqu ymm4, [rdi + rcx - 32]
+        sub rcx, rsi
+        lea rsi, [rip + all_ff_bytes]
+        add rcx, rsi
+        vmovdqu ymm7, [rcx]
+        vpand ymm4, ymm4, ymm7
+        vpsubusb ymm5, ymm4, [rip + all_f4_bytes]
+        vpor ymm0, ymm5, ymm0
+        vpsrlq ymm5, ymm4, 4
+        vpand ymm5, ymm5, [rip + lo_nibble_filter]
+        vmovdqa ymm6, [rip + continuation_length]
+        vpshufb ymm6, ymm6, ymm5
+        vperm2i128 ymm8, ymm3, ymm6, 0x21
+        vpalignr ymm8, ymm6, ymm8, 15
+        vpsubusb ymm8, ymm8, [rip + all_u8_ones]
+        vpaddb ymm8, ymm8, ymm6
+        vperm2i128 ymm7, ymm3, ymm8, 0x21
+        vpalignr ymm7, ymm8, ymm7, 14
+        vpsubusb ymm7, ymm7, [rip + all_u8_twos]
+        vpaddb ymm3, ymm8, ymm7
+        vpcmpgtb ymm8, ymm3, ymm6
+        vpcmpgtb ymm7, ymm6, ymm9 
+        vpcmpeqb ymm8, ymm8, ymm7 
+        vpor ymm0, ymm0, ymm8
+        vperm2i128 ymm1, ymm1, ymm4, 0x21
+        vpalignr ymm1, ymm4, ymm1, 15
+        vpcmpeqb ymm7, ymm1, [rip + all_ed_bytes]
+        vpcmpgtb ymm8, ymm4, [rip + all_9f_bytes]
+        vpand ymm7, ymm7, ymm8
+        vpor ymm0, ymm0, ymm7
+        vpcmpeqb ymm7, ymm1, [rip + all_f4_bytes]
+        vpcmpgtb ymm8, ymm4, [rip + all_8f_bytes]
+        vpand ymm7, ymm7, ymm8
+        vpor ymm0, ymm0, ymm7
+        vperm2i128 ymm2, ymm2, ymm5, 0x21
+        vpalignr ymm2, ymm5, ymm2, 15
+        vmovdqa ymm7, [rip + initial_min_mask]
+        vpshufb ymm7, ymm7, ymm2
+        vpcmpgtb ymm8, ymm7, ymm1
+        vmovdqa ymm7, [rip + second_min_mask]
+        vpshufb ymm7, ymm7, ymm2
+        vpcmpgtb ymm7, ymm7, ymm4
+        vpand ymm7, ymm7, ymm8
+        vpor ymm0, ymm0, ymm7
+process_end:
     # test result
         vptest ymm0, ymm0
         jz result_no_error
