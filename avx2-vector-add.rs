f6028ab550bdc593aa79e87ad64d687717f060ba
luojia65 <me@luojia.cc>
2019-09-02 14:56:28 +0800

    add two vectors

--- avx2-vector-add.rs
+++ avx2-vector-add.rs
@@ -4,10 +4,10 @@ global_asm! { r#"
     .section    .data, ""
     .p2align    4
 x_indexes:
-    .quad 0x0000000100000000
-    .quad 0x0000000300000002
-    .quad 0x0000000500000004
-    .quad 0x0000000700000006
+    .quad 0x0000000400000000
+    .quad 0x0000000C00000008
+    .quad 0x0000001400000010
+    .quad 0x0000001C00000018
 .LCPI1_0:
     .long 0xBF800000
 "# }
@@ -15,21 +15,30 @@ x_indexes:
 struct Point(f32, f32);
 
 fn main() {
-    let a = &[Point(1.0, 2.0), Point(10.0, 20.0)];
-    let b = &[Point(3.0, 6.0), Point(30.0, 60.0)];
+    let a = &[
+        Point(1.0, 2.0), Point(10.0, 20.0), Point(1.0, 2.0), Point(10.0, 20.0), 
+        Point(1.0, 2.0), Point(10.0, 20.0), Point(1.0, 2.0), Point(10.0, 20.0), 
+    ];
+    let b = &[
+        Point(3.0, 6.0), Point(30.0, 60.0), Point(3.0, 6.0), Point(30.0, 60.0), 
+        Point(3.0, 6.0), Point(30.0, 60.0), Point(3.0, 6.0), Point(30.0, 60.0), 
+    ];
     let _ = process(a, b);
 }
 
 //todo
-fn process(a: &[Point], _b: &[Point]) {
+fn process(a: &[Point], b: &[Point]) {
     let out = [0f32; 8];
     unsafe { asm!("
         vmovdqa ymm0, [rip + x_indexes]
-        vbroadcastss ymm2, [rip + .LCPI1_0]
-        vgatherdps ymm1, [rdi + 4 * ymm0], ymm2
-        vmovdqu [rax], ymm1
+        vbroadcastss ymm1, [rip + .LCPI1_0]
+        vgatherdps ymm2, [rdi + 2 * ymm0], ymm1
+        vbroadcastss ymm1, [rip + .LCPI1_0]
+        vgatherdps ymm3, [rsi + 2 * ymm0], ymm1
+        vaddps ymm2, ymm2, ymm3
+        vmovdqu [rax], ymm2
     ":
-    :"{rdi}"(a.as_ptr()),"{rax}"(&out as *const _)
+    :"{rdi}"(a.as_ptr()),"{rsi}"(b.as_ptr()),"{rax}"(&out as *const _)
     ::"intel") };
     for i in 0..8 {
         print!("{} ", out[i])
